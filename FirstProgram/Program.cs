3e7806563c545e301ddb9f2dcd152e7d7a33f2e1
Sasha <sasha.kotseba14@gmail.com>
2018-09-12 09:47:44 +0300

    Добавьте файлы проекта.

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class HoursOfSleep
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + ", how many hours of sleep did you get last night?");
+            int hours = int.Parse(Console.ReadLine());
+
+            if(hours >= 8 && hours < 12 ){
+                Console.WriteLine("that`s good");
+            }
+
+            else if(hours >= 12) {
+                Console.WriteLine("hey man, that`s so much");
+            }
+
+            else if(hours < 8 && hours >= 6) {
+                Console.WriteLine("not bad");
+            }
+
+            else if(hours < 6 && hours >= 0) {
+                Console.WriteLine("that`s bad");
+            }
+
+            else{
+                Console.WriteLine("you could not sleep " + hours);
+            }
+        }
+    }
+}
